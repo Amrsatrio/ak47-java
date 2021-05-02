212f875c1ca37b8a8c74310fc5cbf37f99e9e2d5
Amrsatrio <satrionareswara@yahoo.com>
2021-05-02 11:48:30 +0700

    Delete prefix data on leave

--- src/main/java/com/tb24/discordbot/GuildListeners.kt
+++ src/main/java/com/tb24/discordbot/GuildListeners.kt
@@ -1,9 +1,11 @@
 package com.tb24.discordbot
 
+import com.rethinkdb.RethinkDB.r
 import net.dv8tion.jda.api.events.guild.GuildLeaveEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
-class GuildListeners(client: DiscordBot) : ListenerAdapter() {
+class GuildListeners(private val client: DiscordBot) : ListenerAdapter() {
 	override fun onGuildLeave(event: GuildLeaveEvent) {
+		r.table("prefix").get(event.guild.id).delete().run(client.dbConn)
 	}
 }
