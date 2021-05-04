c945eefa7f6c51a21348bb0d7f19f612e3e8506e
Amrsatrio <satrionareswara@yahoo.com>
2021-05-05 00:44:29 +0700

    dms only bruh

--- src/main/java/com/tb24/discordbot/commands/DeleteMessageCommand.kt
+++ src/main/java/com/tb24/discordbot/commands/DeleteMessageCommand.kt
@@ -5,9 +5,11 @@ import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.LiteralMessage
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType
+import net.dv8tion.jda.api.entities.ChannelType
 
 class DeleteMessageCommand : BrigadierCommand("deletemessage", "Deletes my message in your DMs.", arrayOf("delmsg")) {
 	override fun getNode(dispatcher: CommandDispatcher<CommandSourceStack>): LiteralArgumentBuilder<CommandSourceStack> = newRootNode()
+		.requires { it.isFromType(ChannelType.PRIVATE) }
 		.executes {
 			val message = it.source.message.referencedMessage
 				?: throw SimpleCommandExceptionType(LiteralMessage("Quote my message in here to delete it.")).create()
