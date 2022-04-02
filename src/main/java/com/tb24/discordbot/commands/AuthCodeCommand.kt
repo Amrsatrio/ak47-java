b510cbdbfddf505a8d9760ccf14fd61416951091
m4tonoob <matomaj14@gmail.com>
2022-04-02 08:04:41 +0200

    Authcode to give code directly

--- src/main/java/com/tb24/discordbot/commands/AuthCodeCommand.kt
+++ src/main/java/com/tb24/discordbot/commands/AuthCodeCommand.kt
@@ -1,11 +1,14 @@
 package com.tb24.discordbot.commands
 
+import com.google.gson.JsonObject
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.arguments.StringArgumentType.getString
 import com.mojang.brigadier.arguments.StringArgumentType.greedyString
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.tb24.discordbot.util.performWebApiRequest
 import com.tb24.fn.util.EAuthClient
+import com.tb24.fn.util.getString
 
 class AuthCodeCommand : BrigadierCommand("authcode", "Generates a link to the authorization code generation page.") {
 	override fun getNode(dispatcher: CommandDispatcher<CommandSourceStack>): LiteralArgumentBuilder<CommandSourceStack> = newRootNode()
@@ -16,12 +19,14 @@ class AuthCodeCommand : BrigadierCommand("authcode", "Generates a link to the au
 
 	private fun execute(source: CommandSourceStack, clientId: String? = null): Int {
 		source.ensureSession()
+		source.warnCodeToken()
 		val clientId = if (clientId != null) {
 			EAuthClient.values().firstOrNull { it.name.replace("_", "").equals(clientId, true) }?.clientId ?: clientId
 		} else {
 			source.api.userToken.client_id
 		}
-		source.complete(source.generateUrl("https://www.epicgames.com/id/api/redirect?clientId=$clientId&responseType=code"))
+		source.complete(source.api.performWebApiRequest<JsonObject>("https://www.epicgames.com/id/api/redirect?clientId=$clientId&responseType=code").getString("authorizationCode"))
+		source.complete("⚠ **Do not share the code with anyone else!** This will let them log into your account.\nClient: ${EAuthClient.values().firstOrNull { it.clientId == clientId }?.name ?: clientId}")
 		return Command.SINGLE_SUCCESS
 	}
 }
