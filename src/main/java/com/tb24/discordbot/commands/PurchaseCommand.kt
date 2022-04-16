9b8cc7c89722adfcb7df7efb468d751c0a2608a9
Amrsatrio <satrionareswara@yahoo.com>
2022-04-16 23:19:37 +0700

    Minor tweak to .buy result

--- src/main/java/com/tb24/discordbot/commands/PurchaseCommand.kt
+++ src/main/java/com/tb24/discordbot/commands/PurchaseCommand.kt
@@ -195,7 +195,7 @@ fun purchaseOffer(source: CommandSourceStack, offer: CatalogOffer, quantity: Int
 		val commonCore = profileManager.getProfileData("common_core")
 		val successEmbed = source.createEmbed().setColor(BrigadierCommand.COLOR_SUCCESS)
 			.setTitle("✅ " + L10N.format("purchase.success.title"))
-			.addFieldSeparate(L10N.format("purchase.success.received"), results.toList(), 0) { it.asItemStack().render(showType = true) }
+			.addFieldSeparate(L10N.format("purchase.success.received"), results.toList(), 0) { it.asItemStack().render(showType = true, useDefaultRarityEmote = results.size > 10) }
 			.addField(L10N.format("purchase.success.final_balance"), price.getAccountBalanceText(profileManager), false)
 			.setTimestamp(Instant.now())
 		if (offer.refundable && !isUndoUnderCooldown(commonCore, offer.offerId)) {
